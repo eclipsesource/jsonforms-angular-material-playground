5876033525e016f77ceb2a75c62e8d7f4984d813
Eugen Neufeld <eneufeld@eclipsesource.com>
2018-11-19 16:41:41 +0100

    Show current data
    the current data from the store is shown above the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { select } from '@angular-redux/store';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +8,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @select(['jsonforms', 'core', 'data']) readonly data$: Observable<any>;
 }
