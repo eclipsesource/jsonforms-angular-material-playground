c03dea5e0ea920ff27fb5189fcef21a43291c99b
Edgar Mueller <emueller@eclipsesource.com>
2018-11-26 11:10:27 +0100

    Add custom autocomplete renderer

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,16 +1,22 @@
-import { combineReducers, Reducer } from 'redux';
-import {
-  jsonformsReducer,
-  JsonFormsState,
-} from '@jsonforms/core';
+import {combineReducers, Reducer} from 'redux';
+import {and, jsonformsReducer, JsonFormsState, rankWith, schemaTypeIs, scopeEndsWith, Tester,} from '@jsonforms/core';
 
-import { angularMaterialRenderers } from '@jsonforms/angular-material';
+import {angularMaterialRenderers} from '@jsonforms/angular-material';
+import {CustomAutocompleteControlRenderer} from './custom.autocomplete';
 
 export const rootReducer: Reducer<JsonFormsState> = combineReducers({ jsonforms: jsonformsReducer() });
 
+const departmentTester: Tester = and(
+  schemaTypeIs('string'),
+  scopeEndsWith('department')
+);
+
 export const initialState: any = {
   jsonforms: {
-    renderers: angularMaterialRenderers,
+    renderers: [
+      ...angularMaterialRenderers,
+      { tester: rankWith(5, departmentTester), renderer: CustomAutocompleteControlRenderer }
+    ],
     fields: [],
   }
 };
